5d082692eb558de501338e1d8455ba491573f8f1
Tornike <tornikekapan@gmail.com>
2025-02-07 00:34:33 +0200

    feat: add selector memorization

--- src/selectors/selectFolders.js
+++ src/selectors/selectFolders.js
@@ -1,47 +1,46 @@
+import { createSelector } from '@reduxjs/toolkit'
+
 import { selectRecords } from './selectRecords'
 
-export const selectFolders = (filters) => (state) => {
-  const { isLoading, data: records } = selectRecords({
-    filters: {
-      searchPattern: filters?.searchPattern,
-      isFolder: true
-    }
-  })(state)
-
-  return {
-    isLoading: isLoading,
-    data: records?.reduce(
-      (acc, record) => {
-        const folder = record.folder
-
-        if (record.isFavorite) {
-          acc.favorites.records.push(record)
-
-          return acc
-        }
-
-        if (!folder) {
-          acc.noFolder.records.push(record)
-
-          return acc
-        }
-
-        if (!acc.customFolders?.[folder]) {
-          acc.customFolders[folder] = {
-            name: folder,
-            records: []
-          }
-        }
-
-        acc.customFolders[folder].records.push(record)
-
-        return acc
-      },
-      {
-        favorites: { records: [] },
-        noFolder: { records: [] },
-        customFolders: {}
+export const selectFolders = (filters) =>
+  createSelector(
+    selectRecords({
+      filters: {
+        searchPattern: filters?.searchPattern,
+        isFolder: true
       }
-    )
-  }
-}
+    }),
+    ({ isLoading, data: records }) => ({
+      isLoading,
+      data: records?.reduce(
+        (acc, record) => {
+          const folder = record.folder
+
+          if (record.isFavorite) {
+            acc.favorites.records.push(record)
+            return acc
+          }
+
+          if (!folder) {
+            acc.noFolder.records.push(record)
+            return acc
+          }
+
+          if (!acc.customFolders[folder]) {
+            acc.customFolders[folder] = {
+              name: folder,
+              records: []
+            }
+          }
+
+          acc.customFolders[folder].records.push(record)
+          return acc
+        },
+        {
+          favorites: { records: [] },
+          noFolder: { records: [] },
+          customFolders: {}
+        }
+      )
+    })
+  )
--- src/selectors/selectRecordCountsByType.js
+++ src/selectors/selectRecordCountsByType.js
@@ -1,32 +1,36 @@
+import { createSelector } from '@reduxjs/toolkit'
+
 import { RECORD_TYPES } from '../constants/recordTypes'
 
-export const selectRecordCountsByType = (state) => {
-  const records = state.vault.data?.records ?? []
+export const selectRecordCountsByType = createSelector(
+  (state) => state.vault,
+  (vault) => {
+    const records = vault.data?.records ?? []
 
-  const data = records.reduce(
-    (acc, record) => {
-      const type = record.type
+    const data = records.reduce(
+      (acc, record) => {
+        const type = record.type
 
-      if (!acc[type]) {
-        acc[type] = 0
+        if (!acc[type]) {
+          acc[type] = 0
+        }
+        acc[type]++
+
+        return acc
+      },
+      {
+        all: records.length,
+        [RECORD_TYPES.NOTE]: 0,
+        [RECORD_TYPES.CREDIT_CARD]: 0,
+        [RECORD_TYPES.CUSTOM]: 0,
+        [RECORD_TYPES.IDENTITY]: 0,
+        [RECORD_TYPES.LOGIN]: 0
       }
+    )
 
-      acc[type]++
-
-      return acc
-    },
-    {
-      all: records.length,
-      [RECORD_TYPES.NOTE]: 0,
-      [RECORD_TYPES.CREDIT_CARD]: 0,
-      [RECORD_TYPES.CUSTOM]: 0,
-      [RECORD_TYPES.IDENTITY]: 0,
-      [RECORD_TYPES.LOGIN]: 0
+    return {
+      isLoading: vault.isLoading,
+      data
     }
-  )
-
-  return {
-    isLoading: state.vault.isLoading,
-    data: data
   }
-}
+)
