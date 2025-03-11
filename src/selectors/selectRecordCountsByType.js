d3e9a44f07b6edb3a2f1b6b30d17e34d10d3fd1d
ndolidze <n.dolidze@noxtton.com>
2025-03-11 18:05:24 +0400

    feat: Add filters to useRecordCountsBytype

--- src/selectors/selectRecordCountsByType.js
+++ src/selectors/selectRecordCountsByType.js
@@ -2,36 +2,46 @@ import { createSelector } from '@reduxjs/toolkit'
 
 import { RECORD_TYPES } from '../constants/recordTypes'
 
-export const selectRecordCountsByType = createSelector(
-  (state) => state.vault,
-  (vault) => {
-    const records =
-      vault.data?.records.filter((record) => record.type !== undefined) ?? []
+export const selectRecordCountsByType = ({ filters } = {}) =>
+  createSelector(
+    (state) => state.vault,
+    (vault) => {
+      const records =
+        vault.data?.records.filter((record) => {
+          if (
+            (filters?.folder || filters?.folder === null) &&
+            record.folder !== filters?.folder
+          ) {
+            return false
+          }
 
-    const data = records.reduce(
-      (acc, record) => {
-        const type = record.type
+          return record.type !== undefined
+        }) ?? []
 
-        if (!acc[type]) {
-          acc[type] = 0
+      const data = records.reduce(
+        (acc, record) => {
+          const type = record.type
+
+          if (!acc[type]) {
+            acc[type] = 0
+          }
+          acc[type]++
+
+          return acc
+        },
+        {
+          all: records.length,
+          [RECORD_TYPES.NOTE]: 0,
+          [RECORD_TYPES.CREDIT_CARD]: 0,
+          [RECORD_TYPES.CUSTOM]: 0,
+          [RECORD_TYPES.IDENTITY]: 0,
+          [RECORD_TYPES.LOGIN]: 0
         }
-        acc[type]++
+      )
 
-        return acc
-      },
-      {
-        all: records.length,
-        [RECORD_TYPES.NOTE]: 0,
-        [RECORD_TYPES.CREDIT_CARD]: 0,
-        [RECORD_TYPES.CUSTOM]: 0,
-        [RECORD_TYPES.IDENTITY]: 0,
-        [RECORD_TYPES.LOGIN]: 0
+      return {
+        isLoading: vault.isLoading,
+        data
       }
-    )
-
-    return {
-      isLoading: vault.isLoading,
-      data
     }
-  }
-)
+  )
