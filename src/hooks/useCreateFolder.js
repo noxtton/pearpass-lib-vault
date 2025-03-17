bc7d94d10428e81c95ba84ea4e5b29169fa84c74
Tornike <tornikekapan@gmail.com>
2025-03-18 00:01:14 +0200

    fix: improve folder creation handling

--- src/hooks/useCreateFolder.js
+++ src/hooks/useCreateFolder.js
@@ -1,23 +1,37 @@
 import { useDispatch, useSelector } from 'react-redux'
 
 import { createFolder as createFolderAction } from '../actions/createFolder'
+import { selectFolders } from '../selectors/selectFolders'
 import { selectVault } from '../selectors/selectVault'
 
 /**
  * @param {{
  *  onCompleted?: (payload: {name: string}) => void
+ *  onError?: (error: string) => void
  * }} options
  * @returns {{
  *  isLoading: boolean
  *  createFolder: (folderName: string) => void
  * }}
  */
-export const useCreateFolder = ({ onCompleted } = {}) => {
+export const useCreateFolder = ({ onCompleted, onError } = {}) => {
   const dispatch = useDispatch()
+
+  const { data: foldersData } = useSelector(selectFolders())
 
   const { isFolderLoading: isLoading } = useSelector(selectVault)
 
   const createFolder = async (folderName) => {
+    if (!folderName) {
+      onError?.('folder_name_required')
+      return
+    }
+
+    if (foldersData?.customFolders[folderName]) {
+      onError?.('folder_name_exists')
+      return
+    }
+
     const { error, payload } = await dispatch(createFolderAction(folderName))
 
     if (!error) {
