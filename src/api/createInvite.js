2b2269a8a76c804ec99d507d82f17a0a23e1282f
Tornike Kapanadze <tornikekapan@gmail.com>
2025-04-09 11:10:34 +0300

    Update src/api/createInvite.js
    Co-authored-by: Eugene Glova <eugene.glova@gmail.com>

--- src/api/createInvite.js
+++ src/api/createInvite.js
@@ -4,8 +4,4 @@ import { pearpassVaultClient } from '../instances'
  * @param {string} vaultId
  * @returns {Promise<string>}
  */
-export const createInvite = async () => {
-  const inviteCode = await pearpassVaultClient.activeVaultCreateInvite()
-
-  return inviteCode
-}
+export const createInvite = pearpassVaultClient.activeVaultCreateInvite
