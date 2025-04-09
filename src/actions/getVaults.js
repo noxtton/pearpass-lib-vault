e9b8bed0eaef3bf737fe20cb7c0d8a89fc01d77d
Tornike Kapanadze <tornikekapan@gmail.com>
2025-04-09 11:08:28 +0300

    Update src/actions/getVaults.js
    Co-authored-by: Eugene Glova <eugene.glova@gmail.com>

--- src/actions/getVaults.js
+++ src/actions/getVaults.js
@@ -2,8 +2,4 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 
 import { listVaults } from '../api/listVaults'
 
-export const getVaults = createAsyncThunk('vaults/getVaults', async () => {
-  const vaults = await listVaults()
-
-  return vaults
-})
+export const getVaults = createAsyncThunk('vaults/getVaults', listVaults)
