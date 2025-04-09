6b114eb6042dc81836bb1bdfa41ff041d2373534
Tornike Kapanadze <tornikekapan@gmail.com>
2025-04-09 11:10:45 +0300

    Update src/api/listVaults.js
    Co-authored-by: Eugene Glova <eugene.glova@gmail.com>

--- src/api/listVaults.js
+++ src/api/listVaults.js
@@ -4,7 +4,7 @@ import { pearpassVaultClient } from '../instances'
  * @returns {Promise<Array<any>>}
  */
 export const listVaults = async () => {
-  const vaults = await pearpassVaultClient.vaultsList(`vault/`)
+  const vaults = await pearpassVaultClient.vaultsList('vault/')
 
   return vaults
 }
