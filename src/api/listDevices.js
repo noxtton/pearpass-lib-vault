c2f0dad0f0f0a7da5ad5774b01f7b360d331ad8e
GreedyAI <m.vato122@gmail.com>
2025-06-17 10:53:59 +0400

    Update src/api/listDevices.js
    Co-authored-by: Tornike Kapanadze <tornikekapan@gmail.com>

--- src/api/listDevices.js
+++ src/api/listDevices.js
@@ -3,8 +3,4 @@ import { pearpassVaultClient } from '../instances'
 /**
  * @returns {Promise<Array<Object>>}
  */
-export const listDevices = async () => {
-  const devices = await pearpassVaultClient.activeVaultList(`device/`)
-
-  return devices
-}
+export const listDevices = async () => pearpassVaultClient.activeVaultList(`device/`)
