bde5e63f3da401b421af3ff564b0ddf445394417
Giorgi Shavtvalishvili <62855811+shavtvalishvili@users.noreply.github.com>
2025-06-20 14:17:38 +0400

    chore: add credential to login validation schema (#13)

--- src/utils/validateAndPrepareLoginData.js
+++ src/utils/validateAndPrepareLoginData.js
@@ -5,10 +5,33 @@ import {
   validateAndPrepareCustomFields
 } from './validateAndPrepareCustomFields'
 
+export const credentialSchema = Validator.object({
+  authenticatorAttachment: Validator.string().required(),
+  clientExtensionResults: Validator.object({
+    credProps: Validator.object({
+      rk: Validator.boolean().required()
+    })
+  }).required(),
+  id: Validator.string().required(),
+  rawId: Validator.string().required(),
+  response: Validator.object({
+    attestationObject: Validator.string().required(),
+    authenticatorData: Validator.string().required(),
+    clientDataJSON: Validator.string().required(),
+    publicKey: Validator.string().required(),
+    publicKeyAlgorithm: Validator.number().required(),
+    transports: Validator.array().items(Validator.string()).required()
+  }).required(),
+  type: Validator.string().required(),
+  _privateKey: Validator.string().required(),
+  _userId: Validator.string().required()
+})
+
 export const loginSchema = Validator.object({
   title: Validator.string().required(),
   username: Validator.string(),
   password: Validator.string(),
+  credential: credentialSchema,
   note: Validator.string(),
   websites: Validator.array().items(Validator.string().required()),
   customFields: Validator.array().items(customFieldSchema)
@@ -19,6 +42,7 @@ export const validateAndPrepareLoginData = (login) => {
     title: login.title,
     username: login.username,
     password: login.password,
+    credential: login.credential,
     note: login.note,
     websites: login.websites,
     customFields: validateAndPrepareCustomFields(login.customFields)
--- src/utils/validateAndPrepareLoginData.test.js
+++ src/utils/validateAndPrepareLoginData.test.js
@@ -1,6 +1,31 @@
 import { validateAndPrepareCustomFields } from './validateAndPrepareCustomFields'
 import { validateAndPrepareLoginData } from './validateAndPrepareLoginData'
 
+const mockCredential = {
+  id: 'OLcKHkkaQkO1ik7-DQCejQ',
+  rawId: 'OLcKHkkaQkO1ik7-DQCejQ',
+  type: 'public-key',
+  response: {
+    clientDataJSON: 'eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiZnVWOWxrSS1FZFljTk5aX01HS3FCM2NvM2V1VXhNRkdRVnVVNFpUdWdkcyIsIm9yaWdpbiI6Imh0dHBzOi8vd3d3LnBhc3NrZXlzLWRlYnVnZ2VyLmlvIiwiY3Jvc3NPcmlnaW4iOmZhbHNlfQ',
+    attestationObject: 'o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YViU47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFVdAAAAAAAAAAAAAAAAAAAAAAAAAAAAEDi3Ch5JGkJDtYpO_g0Ano2lAQIDJiABIVggMncC-B3aWkVlxfj8Ir2GxLvvNjsoZ61loh3caiCwKL4iWCD7J3KI1jI0rP8YxAt90voUNiZfE6IMOhWx_QauQMG47A',
+    authenticatorData: '47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFVdAAAAAAAAAAAAAAAAAAAAAAAAAAAAEDi3Ch5JGkJDtYpO_g0Ano2lAQIDJiABIVggMncC-B3aWkVlxfj8Ir2GxLvvNjsoZ61loh3caiCwKL4iWCD7J3KI1jI0rP8YxAt90voUNiZfE6IMOhWx_QauQMG47A',
+    publicKey: 'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEMncC-B3aWkVlxfj8Ir2GxLvvNjsoZ61loh3caiCwKL77J3KI1jI0rP8YxAt90voUNiZfE6IMOhWx_QauQMG47A',
+    publicKeyAlgorithm: -7,
+    transports: [
+      'hybrid',
+      'internal'
+    ]
+  },
+  authenticatorAttachment: 'platform',
+  clientExtensionResults: {
+    credProps: {
+      rk: true
+    }
+  },
+  _privateKey: 'MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgjB33PY-3v-zhNl2CqFQX-i2pwSMr9E1rTJIqu2nZ1BahRANCAAQydwL4HdpaRWXF-PwivYbEu-82OyhnrWWiHdxqILAovvsncojWMjSs_xjEC33S-hQ2Jl8Togw6FbH9Bq5Awbjs',
+  _userId: 'QND9iRzfLqxSDIoB4711RRLXqwISKDmxryYVLW_sEbY'
+}
+
 jest.mock('./validateAndPrepareCustomFields', () => ({
   validateAndPrepareCustomFields: jest.fn((fields) => fields || [])
 }))
@@ -15,6 +40,7 @@ describe('validateAndPrepareLoginData', () => {
       title: 'My Login',
       username: 'user123',
       password: 'password123',
+      credential: mockCredential,
       note: 'This is a note',
       websites: ['https://example.com'],
       customFields: [{ name: 'field1', value: 'value1', type: 'text' }]
@@ -40,6 +66,7 @@ describe('validateAndPrepareLoginData', () => {
       title: 'My Login',
       username: undefined,
       password: undefined,
+      credential: undefined,
       note: undefined,
       websites: ['https://example.com'],
       customFields: []
@@ -84,6 +111,7 @@ describe('validateAndPrepareLoginData', () => {
       title: 'My Login',
       username: null,
       password: undefined,
+      credential: null,
       note: null,
       websites: ['https://example.com'],
       customFields: null
@@ -95,6 +123,7 @@ describe('validateAndPrepareLoginData', () => {
       title: 'My Login',
       username: null,
       password: undefined,
+      credential: null,
       note: null,
       websites: ['https://example.com'],
       customFields: []
