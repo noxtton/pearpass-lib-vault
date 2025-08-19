c5e4a57b0cb1c2bc2bac2c5a0964cf7894717164
Tornike Kapanadze <tornikekapan@gmail.com>
2025-08-19 16:58:16 +0300

    fix: initialisation improvements

--- src/api/init.js
+++ src/api/init.js
@@ -1,4 +1,5 @@
-import { pearpassVaultClient } from '../instances'
+import { initWithCredentials } from './initWithCredentials'
+import { initWithPassword } from './initWithPassword'
 
 /**
  * @param {{
@@ -7,62 +8,22 @@ import { pearpassVaultClient } from '../instances'
  *   hashedPassword: string
  *   password: string
  * }} params
- * @returns {Promise<void>}
+ * @returns {Promise<boolean>}
  */
 export const init = async (params) => {
-  const res = await pearpassVaultClient.vaultsGetStatus()
-
-  if (res?.status) {
-    return true
-  }
-
   if (params?.ciphertext && params?.nonce && params?.hashedPassword) {
-    const decryptVaultKeyRes = await pearpassVaultClient.decryptVaultKey({
+    return initWithCredentials({
       ciphertext: params.ciphertext,
       nonce: params.nonce,
       hashedPassword: params.hashedPassword
     })
-
-    if (!decryptVaultKeyRes) {
-      throw new Error('Error decrypting vault key')
-    }
-
-    await pearpassVaultClient.vaultsInit(decryptVaultKeyRes)
-
-    return true
   }
 
-  if (!params.password) {
-    throw new Error('Password is required')
+  if (params?.password) {
+    return initWithPassword({
+      password: params.password
+    })
   }
 
-  const statusRes = await pearpassVaultClient.encryptionGetStatus()
-
-  if (!statusRes?.status) {
-    await pearpassVaultClient.encryptionInit()
-  }
-
-  const encryptionGetRes =
-    await pearpassVaultClient.encryptionGet('masterPassword')
-
-  const { ciphertext, nonce, salt } = encryptionGetRes
-
-  const hashedPassword = await pearpassVaultClient.getDecryptionKey({
-    salt,
-    password: params.password
-  })
-
-  const decryptVaultKeyRes = await pearpassVaultClient.decryptVaultKey({
-    ciphertext,
-    nonce,
-    hashedPassword
-  })
-
-  if (!decryptVaultKeyRes) {
-    throw new Error('Error decrypting vault key')
-  }
-
-  await pearpassVaultClient.vaultsInit(decryptVaultKeyRes)
-
-  return true
+  throw new Error('Either password or credentials are required')
 }
